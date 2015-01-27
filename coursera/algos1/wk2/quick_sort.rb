2b32b5fb50ff039ed24eb1f0558f1c92fbd97826
Alex Parkinson <okalex@gmail.com>
2015-01-27 11:36:18 -0800

    New quick sort pivot algo

--- coursera/algos1/wk2/quick_sort.rb
+++ coursera/algos1/wk2/quick_sort.rb
@@ -32,13 +32,57 @@ class Array
     self.quick_sort!(pivot_idx + 1, start + len - pivot_idx - 1) unless start + len + 1 == pivot_idx
     self
   end
+
+  def quick_sort2!(start = 0, len = self.size)
+    return if len <= 1
+
+    if len == 2
+      self[start], self[start + 1] = self[start + 1], self[start] if self[start] > self[start + 1]
+      return
+    end
+
+    pivot_idx = start + rand(len)
+    pivot = self[pivot_idx]
+    #puts "(#{start}, #{len}) -> #{self.inspect}, [#{pivot_idx}] = #{pivot}"
+    unless pivot_idx == start
+      self[pivot_idx], self[start] = self[start], self[pivot_idx]
+    end
+
+    left_idx  = start + 1
+    right_idx = start + len - 1
+    while left_idx < right_idx
+      if self[left_idx] > pivot
+        right_idx -= 1 until self[right_idx] < pivot or left_idx >= right_idx
+        unless left_idx == right_idx
+          self[left_idx], self[right_idx] = self[right_idx], self[left_idx]
+          left_idx += 1
+          right_idx -= 1
+        end
+      else
+        left_idx += 1
+      end
+    end
+
+    # Hackey sack
+    if self[left_idx] > pivot
+      left_idx -= 1
+    end
+
+    self[start], self[left_idx] = self[left_idx], self[start]
+    pivot_idx = left_idx
+    #puts "          #{self.inspect}"
+
+    self.quick_sort2!(start, pivot_idx - start) unless start == pivot_idx
+    self.quick_sort2!(pivot_idx + 1, start + len - pivot_idx - 1) unless start + len + 1 == pivot_idx
+    self
+  end
 end
 
 require 'minitest/autorun'
 class TestQuickSort < Minitest::Test
   def test_correctness
     arr = (1..1000).to_a.shuffle
-    arr.quick_sort!
+    arr.quick_sort2!
     prev = 0
     arr.each do |cur|
       assert prev < cur, "Not sorted! #{prev} !< #{cur}"
@@ -49,17 +93,12 @@ end
 
 require 'benchmark'
 Benchmark.bm do |bm|
-  arr = (1..100000).to_a.shuffle
+  arr = (1..1000000).to_a.shuffle
   arr1 = arr.clone
   arr2 = arr.clone
+  arr3 = arr.clone
 
-  bm.report("Ruby sort:") { arr1.sort! }
-  bm.report("Quicksort:") { arr2.quick_sort! }
-end
-
-Benchmark.bm do |bm|
-  (20..24).each do |exp|
-    arr = (1..2**exp).to_a.shuffle
-    bm.report("2^#{exp}:") { arr.sort! }
-  end
+  bm.report("Ruby sort: ") { arr1.sort! }
+  bm.report("Quicksort1:") { arr2.quick_sort! }
+  bm.report("Quicksort2:") { arr3.quick_sort2! }
 end
